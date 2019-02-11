4e5065733ad2763f1c710eeb06537411925b6fde
fxbabin <fbabin@e1r1p8.42.fr>
2019-02-11 17:27:33 +0100

    added vim configuration file

--- install/software_install.sh
+++ install/software_install.sh
@@ -3,3 +3,6 @@
 apt-get update -y && apt-get upgrade -y
 cat requirements.txt | xargs apt-get install -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://github.com/fxbabin/Dotfiles/blob/master/.vimrc?raw=true -O - > ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -E -c PluginInstall -c q -c q; zsh
