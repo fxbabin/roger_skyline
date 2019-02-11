6c00ac4859aa367579b5488deca7a2f45663331e
fxbabin <fbabin@e1r1p8.42.fr>
2019-02-11 17:19:09 +0100

    added requirements.txt

--- install/software_install.sh
+++ install/software_install.sh
@@ -1,8 +1,5 @@
 #! /usr/sh
 
 apt-get update -y && apt-get upgrade -y
-apt-get install vim -y
-printf "sudo\nvim\ncurl\ngit\nzsh\nufw\nportsentry\nfail2ban\napache2\nmailutils" > requirements.txt
-
 cat requirements.txt | xargs apt-get install -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
