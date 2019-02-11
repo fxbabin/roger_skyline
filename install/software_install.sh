477b0e2a90461606d70cba6a57e1818ad00f13e2
fxbabin <fbabin@e1r1p8.42.fr>
2019-02-11 17:15:22 +0100

    starting scripts

--- install/software_install.sh
+++ install/software_install.sh
@@ -1,31 +1,8 @@
-# Debian Software install
+#! /usr/sh
 
-## As root
-
-### Update and install
-
-```
 apt-get update -y && apt-get upgrade -y
 apt-get install vim -y
 printf "sudo\nvim\ncurl\ngit\nzsh\nufw\nportsentry\nfail2ban\napache2\nmailutils" > requirements.txt
-```
 
-set requirements.txt
-
-| tools |
-| --- |
-| sudo |
-| vim |
-| curl |
-| git |
-| zsh |
-| ufw |
-| portsentry |
-| fail2ban |
-| apache2 |
-| mailutils |
-
-```
 cat requirements.txt | xargs apt-get install -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-```
